aabe2589bd7b26a24aae7b636e288775453ebae6
Ted Liou <mail@tedliou.com>
2023-03-16 23:11:05 +0800

    Initial commit

---
+++ Assets/Scripts/ConfigManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using IniParser;
+using IniParser.Model;
+
+namespace IMDLAB.EasyKeyPlayer
+{
+    public class ConfigManager
+    {
+        public ConfigManager()
+        {
+
+        }
+    }
+}
